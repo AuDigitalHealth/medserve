d0520daca1723931681c61f6e778cecb06792616
Steve Crow <Stephen.Crow@nehta.gov.au>
2021-02-11 16:30:24 +1000

    Name change to PBS publication archive

--- server/index-builder/src/main/java/au/gov/digitalhealth/medserve/server/indexbuilder/AmtIndexBuilder.java
+++ server/index-builder/src/main/java/au/gov/digitalhealth/medserve/server/indexbuilder/AmtIndexBuilder.java
@@ -111,7 +111,7 @@ public class AmtIndexBuilder
     }
 
     private String getLatestPbsFileName() {
-        return getYear() + "-" + getMonth() + "-01-extracts-down-converted.zip";
+        return getYear() + "-" + getMonth() + "-01-V2-down-converted.zip";
     }
 
     private String getMonth() {
